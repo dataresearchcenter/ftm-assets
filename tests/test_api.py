e6398492a6dd43a2bfcdc8f3aef78d252efbd296
Simon Wörpel <simon.woerpel@pm.me>
2025-05-10 19:19:09 +0200

    🧪 Fix tests for new model

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from typer.testing import CliRunner
 
 from ftm_assets.api import app
-from ftm_assets.model import Image
+from ftm_assets.model import ApiImageResponse
 
 runner = CliRunner()
 
@@ -12,7 +12,30 @@ def test_api():
 
     res = client.get("/img/Q567")
     assert res.status_code == 200
-    assert Image(**res.json())
+    data = res.json()
+    assert ApiImageResponse(**data)
+    assert data == {
+        "id": "Q567",
+        "name": "Angela Merkel (51614156068).jpg",
+        "url": "https://static.example.org/img/Q567/Angela%20Merkel%20(51614156068).jpg",
+        "alt": [
+            {
+                "text": "Angela Merkel im Jahr 2021 bei der EPP Summit in Brüssel",
+                "language": "de",
+            },
+            {
+                "text": "Angela Merkel in 2021 at the EPP Summit in Brussels",
+                "language": "en",
+            },
+        ],
+        "attribution": {
+            "license": "CC BY 4.0",
+            "license_url": "https://creativecommons.org/licenses/by/4.0/",
+            "author": None,
+        },
+        "original_url": "https://upload.wikimedia.org/wikipedia/commons/0/01/Angela_Merkel_%2851614156068%29.jpg",
+        "thumbnail_url": "https://static.example.org/img/Q567/thumbs/600.jpg",
+    }
 
     res = client.get("/r/img/Q567", follow_redirects=False)
     assert res.status_code == 307
